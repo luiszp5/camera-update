4a617a727c785853602ef87e2f3f40b0b6ac60be
luiszp5 <77802779+luiszp5@users.noreply.github.com>
2022-06-22 11:20:14 -0500

    delete all

--- ios/Pods/Target Support Files/Pods-cameraupdate-cameraupdateTests/Pods-cameraupdate-cameraupdateTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_cameraupdate_cameraupdateTests : NSObject
-@end
-@implementation PodsDummy_Pods_cameraupdate_cameraupdateTests
-@end
--- ios/Pods/Target Support Files/Pods-cameraupdate/Pods-cameraupdate-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_cameraupdate : NSObject
-@end
-@implementation PodsDummy_Pods_cameraupdate
-@end
