72ede3602bdc40b607424833429525cca496b0d4
luiszp5 <77802779+luiszp5@users.noreply.github.com>
2022-06-22 11:14:28 -0500

    new things

---
+++ ios/Pods/Target Support Files/Pods-cameraupdate-cameraupdateTests/Pods-cameraupdate-cameraupdateTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_cameraupdate_cameraupdateTests : NSObject
+@end
+@implementation PodsDummy_Pods_cameraupdate_cameraupdateTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-cameraupdate/Pods-cameraupdate-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_cameraupdate : NSObject
+@end
+@implementation PodsDummy_Pods_cameraupdate
+@end
