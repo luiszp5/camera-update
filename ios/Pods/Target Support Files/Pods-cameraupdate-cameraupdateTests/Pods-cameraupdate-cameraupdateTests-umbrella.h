2bcb4449d2e4e14f4868a508956f40b5b8934474
luiszp5 <77802779+luiszp5@users.noreply.github.com>
2022-06-22 11:29:35 -0500

    Last changes2

---
+++ ios/Pods/Target Support Files/Pods-cameraupdate-cameraupdateTests/Pods-cameraupdate-cameraupdateTests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_cameraupdate_cameraupdateTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_cameraupdate_cameraupdateTestsVersionString[];
+
